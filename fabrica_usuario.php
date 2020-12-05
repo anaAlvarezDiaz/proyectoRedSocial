2a7297fe5c1b22d5c265e9032aeef3af72dbdb91
aalvarez <59690363+anaAlvarezDiaz@users.noreply.github.com>
2020-12-05 17:19:30 +0100

    Create fabrica_usuario.php

---
+++ fabrica_usuario.php
@@ -0,0 +1,35 @@
+<?php
+//include_once('conexion.php');
+include_once('error_handler.php');
+
+include_once('usuario.php');
+include_once('registered_user.php');
+include_once('free_user.php');
+
+
+//clase que creara una instancia de usuario en funcion de si está o no en la base de datos
+class CrearUsuario{
+
+    public static function tipoUsuario($usuario){
+        
+        //comprobar si el usuario se encuentra en la base de datos
+
+        //si se encuentra en la base de datos
+        if ($usuario == 'registrado'){
+            return new Registrado();
+        } else{
+
+            //si no se encuentra en la base de datos
+            return new Invitado();
+        }
+
+    }
+
+}
+
+
+CrearUsuario::tipoUsuario('registrado')->__toString();
+
+
+
+?>
